e02aee8b17eec9381460ec8d82cbb854f6e4aaea
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-06-02 23:19:46 -0400

    comment on router

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// InitalizeRouter initializes all the routes for the application
 func InitalizeRouter(router *gin.Engine) {
 	initializerUserRouter(router)
 }
